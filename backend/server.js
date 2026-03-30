d45ee12a2188e27327c069db234d2f981b782213
Ayush Kumar <ks.ayush2618@gmail.com>
2026-03-30 16:45:56 +0530

    frontend cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://simply-med.vercel.app/",
+      "https://simply-med.vercel.app",
     ],
     credentials: true,
   })
